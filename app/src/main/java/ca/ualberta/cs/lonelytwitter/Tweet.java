8f21629d4a93c4fe9602dca05bc29551d1e6689a
BaihongQi <bq@ualberta.ca>
2015-09-22 13:04:53 -0600

    add a clear button

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -41,5 +41,8 @@ public abstract class Tweet extends Object implements Tweetable {
     }
 
     public abstract Boolean isImportant();
-
+    @Override
+    public String toString(){
+        return date.toString()+ "|" + text;
+    }
 }
